310e23c8aa88571bcee43eee8b29cc711bca8ad2
AnanthuTD <ananthutd2021@gmail.com>
2024-11-10 12:40:44 +0530

    feat: added proxy rewrite to redirect request starting with api to API_URL specified in .env.local file

--- next.config.js
+++ next.config.js
@@ -1,9 +1,21 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {};
+
+const apiUrl = process.env.API_URL;
+console.log("apiUrl: ", apiUrl)
 
 module.exports = {
 	...nextConfig,
 	typescript: {
 		ignoreBuildErrors: true,
+	},
+	async rewrites() {
+		return [
+			{
+				source: "/api/:path*",
+				destination: `${apiUrl}/:path*`,
+			},
+		];
 	},
 };
