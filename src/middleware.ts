e06581ed67bb6634bc684ef1c232ffde4ea32410
AnanthuTD <ananthutd2021@gmail.com>
2024-11-19 19:44:43 +0530

    feat: removed checking for cookies

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,8 +5,8 @@ import type { NextRequest } from "next/server";
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
 	let userCookies = request.cookies.has("user");
-	if (!userCookies)
-		return NextResponse.redirect(new URL("/login", request.url));
+	/* if (!userCookies)
+		return NextResponse.redirect(new URL("/login", request.url)); */
 }
 
 // See "Matching Paths" below to learn more
