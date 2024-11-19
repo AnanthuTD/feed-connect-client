9c6589a9432ca6b25a5d62047c89d72b4819c420
AnanthuTD <ananthutd2021@gmail.com>
2024-11-19 12:31:58 +0530

    feat: replace rest with graphql
    - added infinite scrolling

--- src/app/(main)/inbox/_components/chatBox.tsx
+++ src/app/(main)/inbox/_components/chatBox.tsx
@@ -1,106 +1,115 @@
 "use client";
 import React, { useEffect, useRef, useState } from "react";
-import SmileIcon from "@/app/components/icons/smileIcon";
+import { gql, useMutation, useQuery, useSubscription } from "@apollo/client";
+import { message as antdMessage, Button, Input } from "antd";
+import { SmileOutlined } from "@ant-design/icons";
 import Message from "./message";
 import { Chat } from "@/utils/Interfaces";
-import axios from "@/lib/axios";
-import { message as antdMessage } from "antd";
-
-interface WebSocketData {
-	data: string;
-}
+import { FETCH_MESSAGES, SEND_MESSAGE, SUBSCRIBE_MESSAGE } from "@/graphql/queries";
 
 interface ChatBoxProps {
 	recipient: string; // username
 }
 
 function ChatBox({ recipient }: ChatBoxProps) {
-	const chatLogRef = useRef<HTMLDivElement>(null);
-	const textAreaRef = useRef<HTMLTextAreaElement>(null);
-	const [chatSocket, setChatSocket] = useState<WebSocket | null>(null);
-	const [chats, setChats] = useState<Chat[]>([]);
 	const [message, setMessage] = useState("");
+	const [skip, setSkip] = useState(0);
+	const [take] = useState(20); // Fetch 20 messages at a time
+	const [hasMore, setHasMore] = useState(true);
+	const chatLogRef = useRef<HTMLDivElement>(null);
+	const sentinelRef = useRef(null);
+
+	// Fetch messages with pagination
+	const { data, loading, error, fetchMore } = useQuery(FETCH_MESSAGES, {
+		variables: { receiverId: recipient, take, skip },
+		notifyOnNetworkStatusChange: true,
+	});
+
+	const [chats, setChats] = useState<Chat[]>([]);
 
 	useEffect(() => {
-		const socket = new WebSocket(`ws://localhost:8000/ws/chat/${recipient}/`);
+		if (data?.getMessages) {
+			const { messages, hasMore } = data.getMessages;
 
-		setChatSocket(socket);
+			setChats((prev) => [...messages, ...prev]);
+			setHasMore(hasMore);
+		} else {
+			setHasMore(false);
+		}
+	}, [data?.getMessages]);
 
-		socket.addEventListener("message", (event: WebSocketData) => {
-			const data = JSON.parse(event.data);
-			if (data.hasOwnProperty("status")) return;
-
-			const newMessage: Chat = data.message;
-			setChats((prevChats) => [...prevChats, newMessage]);
-
-			if (chatLogRef.current) {
-				chatLogRef.current.scrollTop = chatLogRef.current.scrollHeight;
+	useSubscription(SUBSCRIBE_MESSAGE, {
+		onData: ({ data }) => {
+			const newMessage = data.data.MessageSubscription;
+			if (newMessage) {
+				setChats((prev) => [...prev, newMessage]);
 			}
-		});
+		},
+		onError: (error) => console.error("Subscription error:", error),
+	});
 
-		socket.addEventListener("close", () => {
-			console.error("Chat socket closed unexpectedly");
-		});
+	useEffect(() => {
+		console.log(chats);
+	}, [chats]);
+
+	const [sendMessage] = useMutation(SEND_MESSAGE, {
+		onCompleted: () => {
+			setMessage("");
+		},
+		onError: (err) => {
+			antdMessage.error("Failed to send message!");
+		},
+	});
+
+	// IntersectionObserver to detect when sentinel is in view
+	useEffect(() => {
+		const observer = new IntersectionObserver(
+			(entries) => {
+				if (entries[0].isIntersecting && hasMore && !loading) {
+					// Load more messages
+					setSkip((prevSkip) => prevSkip + take);
+					fetchMore({ variables: { skip: skip + take } });
+				}
+			},
+			{
+				root: null, // Observe relative to the viewport
+				rootMargin: "0px",
+				threshold: 0.1, // Trigger when 10% of the sentinel is visible
+			}
+		);
+
+		if (sentinelRef.current) {
+			observer.observe(sentinelRef.current);
+		}
 
 		return () => {
-			socket.close();
-		};
-	}, [recipient]);
-
-	useEffect(() => {
-		const fetchMessages = async () => {
-			try {
-				if (!recipient) return;
-
-				const { data }: { data: { message_list: [] } } = await axios.get(
-					`/api/chat/${recipient}/load_messages/`
-				);
-				setChats(data.message_list);
-				console.log("================message_list====================");
-				console.log(data.message_list);
-				console.log("====================================");
-			} catch (error) {
-				console.error("Error during Axios request:", error);
-				antdMessage.warning("Unable to load messages!");
+			if (sentinelRef.current) {
+				observer.unobserve(sentinelRef.current);
 			}
 		};
-
-		fetchMessages();
-	}, [recipient]);
+	}, [hasMore, loading, skip, take, fetchMore]);
 
 	const handleSendMessage = () => {
-		if (chatSocket && chatSocket.readyState === WebSocket.OPEN && message) {
-			chatSocket.send(JSON.stringify({ message }));
-			setMessage("");
+		if (message) {
+			sendMessage({
+				variables: { receiverId: recipient, content: message },
+			});
 		}
 	};
 
 	const handleKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
-		if (event.key === "Enter") {
+		if (event.key === "Enter" && !event.shiftKey) {
+			event.preventDefault();
 			handleSendMessage();
 		}
 	};
 
+	// Handle scroll event
 	useEffect(() => {
 		if (chatLogRef.current) {
 			chatLogRef.current.scrollTop = chatLogRef.current.scrollHeight;
 		}
 	}, [chats]);
-
-	useEffect(() => {
-		if (textAreaRef && textAreaRef.current) {
-			const lineHeight = parseFloat(
-				getComputedStyle(textAreaRef.current).lineHeight
-			);
-			textAreaRef.current.style.maxHeight = lineHeight * 4 + "px";
-			const scrollHeight = textAreaRef.current.scrollHeight;
-			if (lineHeight * 4 >= scrollHeight) {
-				textAreaRef.current.style.height = scrollHeight + "px";
-			} else {
-				textAreaRef.current.style.overflowY = "scroll";
-			}
-		}
-	}, [message]);
 
 	const shouldDisplayDate = (currentTimestamp: Date, nextTimestamp: Date) => {
 		return nextTimestamp.getDate() !== currentTimestamp.getDate();
@@ -138,13 +147,13 @@ function ChatBox({ recipient }: ChatBoxProps) {
 		currentChat: Chat,
 		nextChat: Chat
 	) => {
-		let prevTimestamp = prevChat?.timestamp || undefined;
-		let currentTimestamp = currentChat.timestamp;
-		let nextTimestamp = nextChat?.timestamp || undefined;
+		let prevTimestamp = prevChat?.createdAt || undefined;
+		let currentTimestamp = currentChat?.createdAt;
+		let nextTimestamp = nextChat?.createdAt || undefined;
 
-		let prevMessageSenderUsername = prevChat?.sender_username || undefined;
-		let currentMessageSenderUsername = currentChat.sender_username;
-		let nextMessageSenderUsername = nextChat?.sender_username || undefined;
+		let prevMessageSenderUsername = prevChat?.senderId || undefined;
+		let currentMessageSenderUsername = currentChat.senderId;
+		let nextMessageSenderUsername = nextChat?.senderId || undefined;
 
 		const prevTimestampString = formatTimestamp(prevTimestamp);
 
@@ -168,98 +177,99 @@ function ChatBox({ recipient }: ChatBoxProps) {
 	return (
 		<div className="flex h-full w-full flex-col justify-between">
 			<div className="h-full overflow-y-auto" ref={chatLogRef}>
-				{chats.map((chat, index) => {
-					const currentTimeStamp = new Date(chat.timestamp);
+				<div style={{ height: "100%" }}>
+					<div ref={sentinelRef} style={{ height: "20px" }}></div>
+					{loading ? (
+						<p>Loading messages...</p>
+					) : (
+						chats?.map((chat: Chat, index: number) => {
+							const currentTimeStamp = new Date(chat?.createdAt);
 
-					let prevMessage: Chat;
-					let nextMessage: Chat;
+							let prevMessage: Chat;
+							let nextMessage: Chat;
 
-					if (index > 0) {
-						prevMessage = chats[index - 1];
-					} else {
-						prevMessage = chat;
-					}
+							if (index > 0) {
+								prevMessage = chats[index - 1];
+							} else {
+								prevMessage = chat;
+							}
 
-					if (index === chats.length - 1) {
-						nextMessage = chat;
-					} else {
-						nextMessage = chats[index + 1];
-					}
+							if (index === chats.length - 1) {
+								nextMessage = chat;
+							} else {
+								nextMessage = chats[index + 1];
+							}
 
-					const prevTimestamp = new Date(prevMessage.timestamp);
-					const nextTimestamp = new Date(nextMessage.timestamp);
+							const prevTimestamp = new Date(prevMessage?.createdAt);
+							const nextTimestamp = new Date(nextMessage?.createdAt);
 
-					const displayDate = shouldDisplayDate(
-						currentTimeStamp,
-						nextTimestamp
-					);
-					const displayTime = shouldDisplayTime(
-						currentTimeStamp,
-						nextTimestamp,
-						index
-					);
-					const endNoneRounded = shouldRoundTopNone(
-						chats[index - 1],
-						chat,
-						chats[index + 1]
-					);
+							const displayDate = shouldDisplayDate(
+								currentTimeStamp,
+								nextTimestamp
+							);
+							const displayTime = shouldDisplayTime(
+								currentTimeStamp,
+								nextTimestamp,
+								index
+							);
+							const endNoneRounded = shouldRoundTopNone(
+								chats[index - 1],
+								chat,
+								chats[index + 1]
+							);
 
-					return (
-						<React.Fragment key={chat.id}>
-							{displayDate && (
-								<div className="m-3 flex w-full justify-center">
-									<span className="text-secondaryText text-xs">
-										{currentTimeStamp.toLocaleString("en-IN", {
-											day: "numeric",
-											month: "short",
-											year: "numeric",
-											hour: "numeric",
-											minute: "numeric",
-											hour12: true,
-										})}
-									</span>
-								</div>
-							)}
-							<Message
-								chat={chat}
-								position={
-									chat.sender_username === recipient ? "left" : "right"
-								}
-								key={chat.id}
-								displayTime={displayTime}
-								endNoneRounded={endNoneRounded}
-							/>
-						</React.Fragment>
-					);
-				})}
-			</div>
-
-			<div>
-				<div className="m-3 flex items-center justify-between gap-3 rounded-full p-3 outline outline-1 outline-border_grey">
-					<div className="flex w-full items-center gap-3">
-						<SmileIcon fill="white" width={25} height={25} />
-						<textarea
-							id="chat-message-input"
-							className="w-full resize-none bg-transparent text-sm text-primaryText outline-none"
-							onKeyDown={handleKeyDown}
-							onChange={(e) => setMessage(e.target.value)}
-							value={message}
-							placeholder="Message"
-							rows={1}
-							ref={textAreaRef}
-							style={{ overflowY: "hidden" }}
-						/>
-					</div>
-					{message && (
-						<button
-							id="chat-message-submit"
-							onClick={handleSendMessage}
-							className="text-sm font-bold text-blue-500"
-						>
-							Send
-						</button>
+							return (
+								<React.Fragment key={chat.id}>
+									{displayDate && (
+										<div className="m-3 flex w-full justify-center">
+											<span className="text-secondaryText text-xs">
+												{currentTimeStamp.toLocaleString("en-IN", {
+													day: "numeric",
+													month: "short",
+													year: "numeric",
+													hour: "numeric",
+													minute: "numeric",
+													hour12: true,
+												})}
+											</span>
+										</div>
+									)}
+									<Message
+										chat={chat}
+										position={chat.senderId === recipient ? "left" : "right"}
+										key={chat.id}
+										displayTime={displayTime}
+										endNoneRounded={endNoneRounded}
+									/>
+								</React.Fragment>
+							);
+						})
 					)}
 				</div>
+			</div>
+
+			<div className="m-3 flex items-center justify-between gap-3 rounded-full p-3 outline outline-1 outline-border_grey">
+				<div className="flex w-full items-center gap-3">
+					<SmileOutlined style={{ fontSize: 25, color: "white" }} />
+					<Input.TextArea
+						className="w-full resize-none bg-transparent text-sm text-primaryText outline-none"
+						value={message}
+						onChange={(e) => setMessage(e.target.value)}
+						onKeyDown={handleKeyDown}
+						placeholder="Message"
+						rows={1}
+						style={{ overflowY: "hidden" }}
+					/>
+				</div>
+				{message && (
+					<Button
+						type="link"
+						onClick={handleSendMessage}
+						className="text-sm font-bold text-blue-500"
+					>
+						Send
+					</Button>
+				)}
 			</div>
 		</div>
 	);
