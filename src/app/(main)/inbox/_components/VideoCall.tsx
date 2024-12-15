b0ec0a868c288f0a11ecea1bb7c2f9257dbf5e9a
AnanthuTD <ananthutd2021@gmail.com>
2024-12-15 16:46:03 +0530

    feat: added video call component

--- src/app/(main)/inbox/_components/VideoCall.tsx
+++ src/app/(main)/inbox/_components/VideoCall.tsx
@@ -1,9 +1,36 @@
-import React, { useState } from "react";
+import React, { Profiler, useEffect, useState } from "react";
 import { VideoCameraAddOutlined } from "@ant-design/icons";
-import VideoCallModal from "./videoCallModal";
+import VideoCallModal from "./VideoCallModal";
+import { useQuery } from "@apollo/client";
+import { gql } from "@apollo/client";
 
-function VideoCall({userId}: {userId: string}) {
-	const [isOpen, setIsOpen] = useState(true);
+const GET_USER_PROFILE = gql`
+	query GetUserProfile($username: String, $id: ID) {
+		userProfile(username: $username, id: $id) {
+				id
+				username
+				fullName
+				avatar
+		}
+	}
+`;
+
+function VideoCall({ userId }: { userId: string }) {
+	const [isOpen, setIsOpen] = useState(false);
+
+	/* Get callee user profile */
+	const { data: calleeProfile } = useQuery(GET_USER_PROFILE, {
+		variables: {
+			id: userId,
+		},
+	});
+
+	useEffect(() => {
+		if (calleeProfile?.userProfile?.user) {
+			console.log(calleeProfile?.userProfile?.user);
+			setIsOpen(true);
+		}
+	}, [calleeProfile]);
 
 	const handleOpen = () => {
 		setIsOpen(true);
@@ -13,11 +40,39 @@ function VideoCall({userId}: {userId: string}) {
 		setIsOpen(false);
 	};
 
+	function onRender(
+		id: string,
+		phase: string,
+		actualDuration: number,
+		baseDuration: number,
+		startTime: number,
+		commitTime: number,
+	) {
+		// Aggregate render timings
+		console.log({
+			id,
+			phase,
+			actualDuration,
+			baseDuration,
+			startTime,
+			commitTime,
+		});
+	}
+
 	return (
 		<>
+			<Profiler id="App" onRender={onRender}>
+
 			<VideoCameraAddOutlined style={{ fontSize: 25 }} onClick={handleOpen} />
 
-			{isOpen && <VideoCallModal userId={userId} open={isOpen} onClose={handleClose} />}
+			{isOpen && (
+				<VideoCallModal
+					onClose={handleClose}
+					offerData={null}
+					calleeInfo={calleeProfile?.userProfile}
+					/>
+				)}
+				</Profiler>
 		</>
 	);
 }
